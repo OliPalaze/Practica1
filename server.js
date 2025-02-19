75cd00580fef490f900f4b51ba8aa09b0136b827
OliPalaze <76796443+OliPalaze@users.noreply.github.com>
2025-02-18 22:23:35 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 3000;
 // Serve static files from the web-ui directory
 app.use(express.static(path.join(__dirname, 'web-ui')));
 
+// Handle root route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'web-ui', 'index.html'));
+});
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
